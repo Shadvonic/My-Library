05f043ddac15bff1585a7c789e98fdbfcf8e61e5
Shadvonic <moxeymarcus28@gmail.com>
2023-03-11 08:10:05 -0500

    Initial Commit

---
+++ M4L4-Wrap-Up-Challenge/M4L4_Wrap_Up_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4L4_Wrap_Up_ChallengeApp.swift
+//  M4L4-Wrap-Up-Challenge
+//
+//  Created by Marc Moxey on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct M4L4_Wrap_Up_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
